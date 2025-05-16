f23d99eada3ba4acabb07a06c7e3eedd7912089d
codesmith <amn2003ktr@gmail.com>
2025-05-16 20:58:21 +0530

    logic added for etch sketch

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const buttonForInput = document.querySelector("#btn");
+// let errorMsg;
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div")
-  div.className = "grid"
-  container.appendChild(div)
+buttonForInput.addEventListener("click", () => {
+  const input = parseInt(prompt("Enter the number of square per side between 1 to 100"))
+  if (input > 100) {
+    errorMsg = document.createElement("h1");
+    errorMsg.id = "err"
+    errorMsg.innerText = "invalid input"
+    document.body.insertBefore(errorMsg, container)
 
-}
+  }
+  else {
 
+    const existingError = document.querySelector("#err");
+    if (existingError) {
+      existingError.remove()
+    }
+    container.innerHTML = ""
+    loopInput = (input * input);
+    const squareSize = Math.floor(960 / input);
 
+    for (let i = 0; i < loopInput; i++) {
+      const divOfGrid = document.createElement("div")
+      divOfGrid.className = "grid";
+      divOfGrid.style.width = `${squareSize}px`;
+      divOfGrid.style.height = `${squareSize}px`;
+      container.appendChild(divOfGrid)
+    }
+  }
+
+})
+container.addEventListener("mouseover", (e) => {
+  if (e.target.className === "grid") {
+    e.target.style.backgroundColor = "black";
+  }
+})
