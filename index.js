c4eac7f7b005f292c42159b73bea2689db502b6b
codesmith <amn2003ktr@gmail.com>
2025-05-16 20:55:58 +0530

    Add button for input in index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container")
+
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement("div")
+  div.className = "grid"
+  container.appendChild(div)
+
+}
+
+
